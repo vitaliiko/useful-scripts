91c6755b5c04567e66f484d5c8389ebcd0245c04
Vitalii Kobrin <vitaliy.kobrin@gmail.com>
2018-12-15 21:40:45 +0200

    Update install-docker.sh

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -10,7 +10,7 @@ sudo apt-get update
 sudo apt-get install docker-ce
 
 # https://docs.docker.com/compose/install/
-sudo curl -L https://github.com/docker/compose/releases/download/1.20.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.23.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # https://docs.docker.com/install/linux/linux-postinstall/
