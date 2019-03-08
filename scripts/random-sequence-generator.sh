b50e48acdd09a2d7e2219c9cc0a8f75df88c4515
Vitalii Kobrin <vitaliy.kobrin@gmail.com>
2019-03-08 18:49:15 +0200

    Update

--- scripts/random-sequence-generator.sh
+++ scripts/random-sequence-generator.sh
@@ -5,5 +5,5 @@ character_set=${2:-'a-zA-Z0-9-_'}
  
 cat /dev/urandom | tr -dc $character_set | fold -w $string_length | head -n 1
 
-# Result exxample: 
+# Result example: 
 # AxZta_aP9JKlq3StM9WlSZ7rgyXvdSpksJF1TIyococzZMMy0B54bVKckV_ruvFD
