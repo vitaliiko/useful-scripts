da360d21856f747c597083858e40104590da81d9
Vitalii Kobrin <vitaliy.kobrin@gmail.com>
2019-03-08 18:49:15 +0200

    Update

--- scripts/docker-size.sh
+++ scripts/docker-size.sh
@@ -1,14 +1,29 @@
 #!/bin/sh
 
-# List all docker containers + volume sizes
+# List all docker containers  + volume sizes
+#
+# Usage: `./docker-size.sh -a` to print all containers
+# `./docker-size.sh "-a -f "name=smart""` to filter containers by name
 
-for d in `docker ps -a | awk '{print $1}' | tail -n +2`; do
-    d_name=`docker inspect -f {{.Name}} $d`
-    echo "========================================================="
-    echo "$d_name ($d) container size:"
-    sudo du -d 2 -h /var/lib/docker/aufs | grep `docker inspect -f "{{.Id}}" $d`
-    echo "$d_name ($d) volumes:"
+container_filter=$1
 
+volumes=$(docker system df -v | awk '/Local Volumes space usage:/,/Build cache usage:/' | tail -n +4 | head -n $(($Count -2)) | awk '{print "ID: "$1" Size: "$3}') 
+containers=$(docker container ls $1 --format "{{.ID}} {{.Names}} :{{.Size}}" | tail -n +1)
 
-    docker inspect -f "{{.Volumes}}" $d | sed 's/map\[//' | sed 's/]//' | tr ' ' '\n' | sed 's/.*://' | xargs sudo du -d 1 -h
+echo "$containers" | while IFS= read -r container ; do 
+	container_id=$(echo $container | awk '{print $1}')
+	container_name=$(echo $container | awk '{print $2}')
+	container_size=$(echo $container | awk -F'[:]' '{print $2}')
+	container_mounts=$(docker inspect --format "{{.Mounts}}" $container_id)
+	
+	echo CONTAINER: $container_name $container_size
+	echo VOLUMES:
+	echo "$volumes" | awk '{print $2}' | while IFS= read -r volume ; do 
+		if [ "$(echo $container_mounts | grep $volume)" ] 
+		then
+			echo "$volumes" | grep $volume
+		fi
+	done
+	echo
 done
+
