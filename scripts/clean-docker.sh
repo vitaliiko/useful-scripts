d19501fa38f6773e50ea22f24a2905d22eb6c76f
vitaliikobrin <vitaliy.kobrin@gmail.com>
2020-04-04 19:49:37 +0300

    Update clean-docker script

--- scripts/clean-docker.sh
+++ scripts/clean-docker.sh
@@ -5,4 +5,5 @@ old_containers_filter=${1:-'weeks ago'}
 docker ps --filter "status=exited" | grep $old_containers_filter | awk '{print $1}' | xargs --no-run-if-empty docker rm
 docker images | grep $old_containers_filter | awk '{print $3}' | xargs --no-run-if-empty docker rmi
 docker image prune
+docker rmi $(docker images -f "dangling=true" -q)  
 docker volume prune
