32936cd39bbc167f789c6a9bc70596954af086cb
Vitalii Kobrin <vitaliy.kobrin@gmail.com>
2020-08-10 15:28:52 +0300

    Update script

--- scripts/download-from-ftp.sh
+++ scripts/download-from-ftp.sh
@@ -10,7 +10,11 @@ file_name_without_extension=$(echo $target_file_name | awk -F. '{print $1}')
 unzip_result_folder=${unzip_folder}/${file_name_without_extension}_$(date '+%H_%M')
 
 mkdir $unzip_result_folder
-echo wget --user=$username --password=$password $base_path/$target_file_name
+cd $unzip_result_folder
+
+echo Downloading $base_path/$target_file_name file
 wget --user=$username --password=$password $base_path/$target_file_name
-unzip $target_file_name -d $unzip_result_folder
+
+echo Unziping archive
+unzip $target_file_name
 
