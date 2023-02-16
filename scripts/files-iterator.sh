4102d2c7408fa735b7eddc94c1febd7d454febaf
Vitalii Kobrin <vitaliy.kobrin@gmail.com>
2023-02-16 23:33:29 +0200

    Update files iterator script

--- scripts/files-iterator.sh
+++ scripts/files-iterator.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-# Iterate through all files in a directory
 DIR='directory/'
+
+# Iterate through all files in a directory
 for FILE in $(ls "$DIR")
 do
     echo $FILE
@@ -9,6 +10,12 @@ done
 
 # Iterate through all files in a directory excluding files with a certain extension
 for FILE in $(ls -p "$DIR" | grep -v .bz2)
+do
+    echo $FILE
+done
+
+# Iterate all files in a directory recursively
+for FILE in $(find "$DIR")
 do
     echo $FILE
 done
