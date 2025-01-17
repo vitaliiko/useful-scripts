44aa07c9d1918e420a873569eee7c5a875932c3f
Vitalii Kobrin <vitaliy.kobrin@gmail.com>
2025-01-17 10:54:32 +0200

    Update performance test

--- scripts/performance-test.sh
+++ scripts/performance-test.sh
@@ -10,14 +10,11 @@ do
     start_time=$(date +%s)
 
     echo Result:
-    # curl -X POST -H "Content-Type: application/json" -d "{\"fromDate\": \"2024-05-14T15:05:32.464Z\", \"itemsPerPage\": $limit, \"pageNumber\": $offset, \"matchingResult\": \"NOT_MATCHED\"}" $endpoint
     curl -X POST -H "Content-Type: application/json" $endpoint -d @- <<BODY
     {
   "example": []
 }
 BODY
-
-
     end_time=$(date +%s)
     execution_time=$(($end_time-$start_time))
     echo "Execution time: $execution_time seconds"
